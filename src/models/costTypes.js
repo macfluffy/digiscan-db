b47871860dbae86d938af27f84a86f58baf060b6
macfluffy <shadowimitation@hotmail.com>
2026-02-27 01:31:29 +1100

    feat: Added the cost types model

---
+++ src/models/costTypes.js
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../index';
+
+// These are the different costs associated with playing a card,
+// referencing the use cost for options and play cost for 
+// everything else
+export class CostTypes extends Model {}
+
+CostTypes.init(
+    {
+        // Model attributes:
+        // Primary key is already assumed and doesn't need to be defined
+        cardType: {
+            type: DataTypes.ENUM,
+            values: [
+                'play', 
+                'use', 
+            ],
+        },
+    },
+    {
+        // Other model options:
+        sequelize,
+        modelName: 'CostTypes',
+    },
+);
