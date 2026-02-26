f0e12a39c6d36dd9e248d10346fa2fe04df8d3fe
macfluffy <shadowimitation@hotmail.com>
2026-02-27 00:55:03 +1100

    feat: Created the cardtyping junction table. This defines the many to many relationship between the cards and types tables

---
+++ src/models/cardTyping.js
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../index';
+
+import { Cards } from './cards';
+import { CardTypes } from './cardTypes';
+
+// This is the card type(s) printed on a specific card
+export class CardTyping extends Model {}
+
+CardTyping.init(
+    {
+        // Model attributes:
+        cardId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Cards,
+                key: 'id',
+            },
+        },
+        typeId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: CardTypes,
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: 'CardTyping',
+    },
+);
+
+// Define the many-to-many relationship between cards and card types
+Cards.belongsToMany(CardTypes, { through: CardTyping });
+CardTypes.belongsToMany(Cards, { through: CardTyping });
