1688def85a369f234db257220d46b9e9e31853a0
macfluffy <shadowimitation@hotmail.com>
2026-02-27 15:06:20 +1100

    feat: added the card special printing junction table, this is used for defining if a card is a regular art or a alternate art card

---
+++ src/models/cardSpecialPrinting.js
@@ -0,0 +1,39 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../index';
+
+import { Cards } from './cards';
+import { CardTreatments } from './cardTreatments';
+
+// These are the type of art cards are printed with.
+// These define if the card is found with only regular 
+// art or some special parallel rare art
+export class CardSpecialPrinting extends Model {}
+
+CardSpecialPrinting.init(
+    {
+        // Model attributes:
+        cardId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Cards,
+                key: 'id',
+            },
+        },
+        treatmentId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: CardTreatments,
+                key: 'id',
+            },
+        },
+    },
+    {
+        // Other model options:
+        sequelize,
+        modelName: 'CardSpecialPrinting',
+    },
+);
+
+// Define the many-to-many relationship between cards and card frames
+Cards.belongsToMany(CardTreatments, { through: CardSpecialPrinting });
+CardTreatments.belongsToMany(Cards, { through: CardSpecialPrinting });
