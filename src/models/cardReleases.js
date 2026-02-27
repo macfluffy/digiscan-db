aa1e11257453af412b681cff77073778d4a9ed58
macfluffy <shadowimitation@hotmail.com>
2026-02-27 14:38:36 +1100

    feat: Added the card releases junction table, this table is for showing where to obtain cards

---
+++ src/models/cardReleases.js
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../index';
+
+import { Cards } from './cards';
+import { CardSets } from './cardSets';
+
+// This table is showing where the cards can be 
+// obtained or sourced from
+export class CardReleases extends Model {}
+
+CardReleases.init(
+    {
+        // Model attributes:
+        cardId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Cards,
+                key: 'id',
+            },
+        },
+        setId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: CardSets,
+                key: 'id',
+            },
+        },
+    },
+    {
+        // Other model options:
+        sequelize,
+        modelName: 'CardReleases',
+    },
+);
+
+// Define the many-to-many relationship between cards and card types
+Cards.belongsToMany(CardSets, { through: CardTyping });
+CardSets.belongsToMany(Cards, { through: CardTyping });
