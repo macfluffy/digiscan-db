acf3cf9e75a6b5be87c0eccc67b72a4566eb8258
macfluffy <shadowimitation@hotmail.com>
2026-05-10 23:33:16 +1000

    feat: New Config file to house the process.env variables. This will be a single source of truth, and does not need to be scattered across all over the code

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,23 @@
+const expressPort = process.env.EXPRESS_PORT;
+const databaseURI = process.env.DATABASE_URI;
+const databaseName = process.env.DATABASE_NAME;
+const databaseUser = process.env.DATABASE_USER;
+const databasePassword = process.env.DATABASE_PASSWORD;
+const databaseHost = process.env.DATABASE_HOST;
+const databasePort = process.env.DATABASE_PORT;
+
 // Validate express port is a number if provided
-if (process.env.EXPRESS_PORT && isNaN(Number(process.env.EXPRESS_PORT))) {
+if (expressPort && isNaN(Number(expressPort))) {
     console.error('Error: Express Port must be a valid number.');
     process.exit(1);
 }
 
 export const config = {
-    port: Number(process.env.EXPRESS_PORT) || 3000,
-    databaseURI: process.env.DATABASE_URI,
+    serverPort: Number(expressPort) || 3000,
+    databaseURI: databaseURI,
+    databaseName: databaseName,
+    databaseUser: databaseUser,
+    databasePassword: databasePassword,
+    databasePort: Number(databasePort) || 5432,
+    databaseHost: databaseHost,
 };
