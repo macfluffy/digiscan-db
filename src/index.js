6cc6f6ed38afbf12883b88db566a52ee3484b668
macfluffy <shadowimitation@hotmail.com>
2026-05-27 17:25:48 +1000

    fix: Actually removed unused imports and comments in the express server index file.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import { sequelize } from './config/database.js';
-import { config } from './config/index.js';
 
 const app = express();
 
@@ -10,12 +8,5 @@ app.use(express.json()); // Parse JSON bodies
 app.get('/', (request, response) => {
     response.send('Hello World!');
 });
-
-// try {
-//   await sequelize.authenticate();
-//   console.log('Connection has been established successfully.');
-// } catch (error) {
-//   console.error('Unable to connect to the database:', error);
-// }
 
 export default app;
