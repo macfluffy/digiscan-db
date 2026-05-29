332e5142f72dbaffa8266da4a4ba3b297eacd89a
macfluffy <shadowimitation@hotmail.com>
2026-05-29 14:00:56 +1000

    feat: Added manual colour seeding script

---
+++ src/utilities/seeders/manual/manual-colours-seed.js
@@ -0,0 +1,33 @@
+import { sequelize } from "../../../config/database.js";
+import { Colours } from "../../../models/colours.js";
+
+const seedData = [
+    { colourName: 'red' },
+    { colourName: 'blue' },
+    { colourName: 'yellow' },
+    { colourName: 'green' },
+    { colourName: 'black' },
+    { colourName: 'purple' },
+    { colourName: 'white' },
+];
+
+export const seedColoursData = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Database connection established.");
+
+        await sequelize.sync({ force: true });
+        console.log("Forcing synchronization.");
+
+        await Colours.bulkCreate(seedData);
+        console.log("Colours table seeded.");
+    } 
+    catch (error) {
+        console.error("Could not initialise Card Types Table: ", error);
+    }
+    finally {
+        await sequelize.close();
+    }
+};
+
+seedColoursData();
