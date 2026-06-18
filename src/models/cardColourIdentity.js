96763573c7476c03e5cf476929a27ace7990d22c
macfluffy <shadowimitation@hotmail.com>
2026-06-18 21:36:55 +1000

    fix: Added the imports back, they were needed. Instead correctly imported them by explicitly calling the file.

--- src/models/cardColourIdentity.js
+++ src/models/cardColourIdentity.js
@@ -1,6 +1,9 @@
 import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../config/database/database';
 
+import { Cards } from './cards.js';
+import { Colours } from './colours.js';
+
 // This defines all the colours a card is. A card can be a 
 // single colour or many colours. This will depend on the 
 // number of times a card appears in this table
