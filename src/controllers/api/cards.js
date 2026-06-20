5b42c2b756068f80091c793ca4851758a933a3f6
macfluffy <shadowimitation@hotmail.com>
2026-06-21 02:13:50 +1000

    feat: Added the colours and card traits into the get all cards response body

--- src/controllers/api/cards.js
+++ src/controllers/api/cards.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import { Cards } from "../../models/cards.js";
 import { CardTypes } from "../../models/cardTypes.js";
+import { Colours } from "../../models/colours.js";
+import { CardTraits } from "../../models/cardTraits.js";
 
 export const cardsRouter = Router();
 
@@ -14,13 +16,27 @@ cardsRouter.get("/", async (request, response) => {
             attributes: {
                 exclude: ['createdAt', 'updatedAt']
             },
-            include: {
+            include: [{
                 model: CardTypes,
-                as: 'cardtypes',
+                as: 'card_types',
                 attributes: ['cardType'],
                 required: true,
                 through: { attributes: [] }
+            }, 
+            {
+                model: Colours,
+                as: 'colours',
+                attributes: ['colourName'],
+                // required: true,
+                through: { attributes: [] }
+            },
+            {
+                model: CardTraits,
+                as: 'card_traits',
+                attributes: ['cardTrait'],
+                through: { attributes: [] }
             }
+        ]
         });
         response.status(200).json(cards);
     }
