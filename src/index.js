fc0bcff60735d7f70355fdf2694a9426d6229802
macfluffy <shadowimitation@hotmail.com>
2026-09-17 19:02:14 +1000

    feat: Added helmet security to protect the users from 14 different exploits. This is set to the default helmet settings (strictest) and the rules will be customised as more third party features are added on.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import helmet from "helmet";
+
 import { mainRouter } from "./controllers/index.js";
 
 const app = express();
 
 // Middlewares
-app.use(express.json()); // Parse JSON bodies
+// Apply security middlewares before the router
+app.use(helmet());
+
+// Parse JSON bodies and then attach the router
+app.use(express.json()); 
 app.use(mainRouter);
 
 // Load in the model associations
