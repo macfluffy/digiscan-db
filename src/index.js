4efd150cc316f1726dca05e782ba73cd85232e8a
macfluffy <shadowimitation@hotmail.com>
2026-09-17 22:20:12 +1000

    feat: Added rate limiting security to the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 import { mainRouter } from "./controllers/index.js";
 
 const app = express();
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,   // 15 minutes
+    limit: 100,                 // Limit each IP 100 requests per window
+});
 
 // Middlewares
 // Apply security middlewares before the router
 app.use(helmet());
+app.use(limiter);
 
 // Parse JSON bodies and then attach the router
 app.use(express.json()); 
